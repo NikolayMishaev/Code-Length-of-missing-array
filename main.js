881707e55ffaa38ba91636725204180bf5d78ac4
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2025-03-06 18:40:50 +0300

    добавлена функция по поиску массива с неверной длиной

--- main.js
+++ main.js
@@ -8,8 +8,8 @@
 псевдокод:
 проверить, что на входе массив со значениями
 проверить, что каждый элемент массива имеет массив со значениями
-    отсортировать элементы массива по их длине
-сравнить длину соседних массивов, и если длина отличается больше чем на один, вернуть этот массив
+отсортировать элементы массива по их длине
+    сравнить длину соседних массивов, и если длина отличается больше чем на один, вернуть этот массив
 отнять от длины найденного массива 1 и вернуть значение
 
 bugfix:
@@ -17,12 +17,17 @@ bugfix:
 */
 
 function getLengthOfMissingArray(arrayOfArrays) {
+
     const isValid = (arr) => arr && arr.length ? arr : 0;
     const sortByLength = (a, b) => a.length - b.length
+    const findArrayFollowingMissingArray = (el, i, arr) => i === 0 ? false : el.length - 1 !== arr[i - 1].length 
+
     if ( isValid(arrayOfArrays) && arrayOfArrays.every(isValid)) {
-        return arrayOfArrays.sort(sortByLength)
+        return arrayOfArrays
+        .sort(sortByLength)
+        .find(findArrayFollowingMissingArray)
     }
 }
 
-console.log(getLengthOfMissingArray([[2,2,3],[3,3,3,3,3,33,,],[4,4],[3]]))
-// console.log(getLengthOfMissingArray([[2],[]]))
+console.log(getLengthOfMissingArray([[2,2,3],[3,3,3,3,3,33],[4,4],[3]]))
+console.log(getLengthOfMissingArray([[1],[1,2,3],[1,2,3,4,5],[1,2],[1,2,3,4,5,6]]))
