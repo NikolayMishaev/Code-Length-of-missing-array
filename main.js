e962f9b2a4a376fc49afa7aa4cf66340a651a25e
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2025-03-06 18:35:46 +0300

    feat: добавлена сортировка массивов по длине

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 псевдокод:
 проверить, что на входе массив со значениями
 проверить, что каждый элемент массива имеет массив со значениями
-отсортировать элементы массива по их длине
+    отсортировать элементы массива по их длине
 сравнить длину соседних массивов, и если длина отличается больше чем на один, вернуть этот массив
 отнять от длины найденного массива 1 и вернуть значение
 
@@ -18,8 +18,11 @@ bugfix:
 
 function getLengthOfMissingArray(arrayOfArrays) {
     const isValid = (arr) => arr && arr.length ? arr : 0;
-    return arrayOfArrays.every(isValid)
+    const sortByLength = (a, b) => a.length - b.length
+    if ( isValid(arrayOfArrays) && arrayOfArrays.every(isValid)) {
+        return arrayOfArrays.sort(sortByLength)
+    }
 }
 
-console.log(getLengthOfMissingArray([[2],[4]]))
-console.log(getLengthOfMissingArray([[2],[]]))
+console.log(getLengthOfMissingArray([[2,2,3],[3,3,3,3,3,33,,],[4,4],[3]]))
+// console.log(getLengthOfMissingArray([[2],[]]))
