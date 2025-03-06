93f6243441dfc5fc7cdf562b775aecc89e436ac7
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2025-03-06 18:46:28 +0300

    feat: функционал реализован

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /* 
 
@@ -9,7 +9,7 @@
 проверить, что на входе массив со значениями
 проверить, что каждый элемент массива имеет массив со значениями
 отсортировать элементы массива по их длине
-    сравнить длину соседних массивов, и если длина отличается больше чем на один, вернуть этот массив
+сравнить длину соседних массивов, и если длина отличается больше чем на один, вернуть этот массив
 отнять от длины найденного массива 1 и вернуть значение
 
 bugfix:
@@ -18,16 +18,21 @@ bugfix:
 
 function getLengthOfMissingArray(arrayOfArrays) {
 
-    const isValid = (arr) => arr && arr.length ? arr : 0;
-    const sortByLength = (a, b) => a.length - b.length
-    const findArrayFollowingMissingArray = (el, i, arr) => i === 0 ? false : el.length - 1 !== arr[i - 1].length 
+  const isValid = (arr) => (arr && arr.length ? arr : 0);
+  const sortByLength = (a, b) => a.length - b.length;
+  const findArrayFollowingMissingArray = (el, i, arr) => i === 0 ? false : el.length - 1 !== arr[i - 1].length;
 
-    if ( isValid(arrayOfArrays) && arrayOfArrays.every(isValid)) {
-        return arrayOfArrays
-        .sort(sortByLength)
-        .find(findArrayFollowingMissingArray)
-    }
+  if (isValid(arrayOfArrays) && arrayOfArrays.every(isValid)) {
+
+    const arrayFollowingMissingArray = arrayOfArrays
+      .sort(sortByLength)
+      .find(findArrayFollowingMissingArray);
+
+    const lengthMissingArray = arrayFollowingMissingArray.length - 1;
+    return lengthMissingArray;
+
+  }
 }
 
-console.log(getLengthOfMissingArray([[2,2,3],[3,3,3,3,3,33],[4,4],[3]]))
-console.log(getLengthOfMissingArray([[1],[1,2,3],[1,2,3,4,5],[1,2],[1,2,3,4,5,6]]))
+console.log(getLengthOfMissingArray([[3, 3, 3], [4, 4, 4, 4], [1]]));
+console.log(getLengthOfMissingArray([[1],[3, 3, 3],[5, 5, 5, 5, 5],[2, 2],[6, 6, 6, 6, 6, 6],]));
